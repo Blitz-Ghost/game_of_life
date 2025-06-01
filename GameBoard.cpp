f54e505ab26679a7cbb8b365d2f1670dc18f4f46
KacperWilk1410 <162425030+KacperWilk1410@users.noreply.github.com>
2025-06-01 22:22:27 +0200

    Adding zero objects and default neighbourhood

---
+++ GameBoard.cpp
@@ -0,0 +1,114 @@
+#include "GameBoard.h"
+#include <cstdlib>
+#include <stdexcept>
+
+GameBoard::GameBoard(size_t cols, size_t rows, unique_ptr<Neighborhood> init_neighborhood, unique_ptr<Boundary> init_boundary, unique_ptr<LiveRules> init_rules)
+    :columns(cols), records(rows), neighborhood(move(init_neighborhood)), boundary(move(init_boundary)), rules(move(init_rules)) {
+    Board.resize(columns * records);
+}
+
+
+GameBoard::GameBoard(size_t cols, size_t rows)
+    : GameBoard(cols, rows,
+        std::make_unique<MooreNeighborhood>(),
+        std::make_unique<DeadOutside>(),
+        std::make_unique<StanrdardConway>()) {}
+
+Cell& GameBoard::at(size_t r, size_t c) {
+    static Cell nullCell; 
+    if (r >= records || c >= columns) {
+        return nullCell;
+    }
+    return Board[r * columns + c];
+}
+
+const Cell& GameBoard::at(size_t r, size_t c) const {
+    static Cell nullCell;
+    if (r >= records || c >= columns) {
+        return nullCell;
+    }
+    return Board[r * columns + c];
+}
+
+size_t GameBoard::GetColumns() const {
+    return columns;
+}
+
+size_t GameBoard::GetRecords() const {
+    return records;
+}
+
+void GameBoard::ChangeCellStatus(size_t r, size_t c) {
+    bool current = at(r, c).getStatus();
+    at(r, c).setStatus(!current);
+}
+
+
+void GameBoard::RewriteFrom(const BufferBoard& copy) {
+    for (size_t i = 0; i < records; i++) {
+        for (size_t j = 0; j < columns; j++) {
+            this->at(i, j).setStatus(copy.at(i, j).getStatus());
+        }
+    }
+}
+
+void GameBoard::checkBoard(BufferBoard& temp) {
+    for (size_t i = 0; i < records; i++) {
+        for (size_t j = 0; j < columns; j++) {
+            
+            bool alive = at(i, j).getStatus();
+            int live_neighbors = neighborhood->countAliveNeighbors(boundary.get(), *this, i, j);
+            bool newStatus = rules->changeedStatus(alive, live_neighbors);
+            temp.at(i, j).setStatus(newStatus);
+        }
+    }
+
+    RewriteFrom(temp);
+}
+
+void GameBoard::randomGen() {
+    for (size_t i = 0; i < records; i++) 
+        for (size_t j = 0; j < columns; j++) {
+            if (rand() % 7 == 1)
+                at(i, j).setStatus(true);
+            else
+                at(i, j).setStatus(false);
+        }
+}
+
+void GameBoard::reset() {
+    for (auto& cell : Board) cell.setStatus(false);
+}
+
+void GameBoard::setBoundary(unique_ptr<Boundary> newBoundary) {
+    boundary = move(newBoundary);
+}
+
+void GameBoard::setNeighborhood(unique_ptr<Neighborhood> newNeighborhood) {
+    neighborhood = move(newNeighborhood);
+}
+
+void GameBoard::setLiveRules(unique_ptr<LiveRules> newRules) {
+    rules = move(newRules);
+}
+
+
+Cell& BufferBoard::at(size_t r, size_t c) {
+    static Cell nullCell;
+    if (r >= records || c >= columns) {
+        return nullCell;
+    }
+    return Board[r * columns + c];
+}
+
+const Cell& BufferBoard::at(size_t r, size_t c) const {
+    static Cell nullCell;
+    if (r >= records || c >= columns) {
+        return nullCell;
+    }
+    return Board[r * columns + c];
+}
+
+
+
+
---
+++ GameBoard.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <vector>
+#include <cstdlib>
+#include "Cell.h"
+#include "LiveRules.h"
+#include "Boundary.h"
+#include "Neighbor.h"
+#include <memory>
+
+
+
+using namespace std;
+
+class BufferBoard {
+private:
+	vector<Cell> Board;
+	size_t columns;
+	size_t records;
+
+public:
+	BufferBoard(size_t cols, size_t rows) : columns(cols), records(rows), Board(cols* rows) {}
+
+	Cell& at(size_t r, size_t c);
+
+	const Cell& at(size_t r, size_t c) const;
+
+};
+
+class BoardAccessor {
+public:
+	virtual const Cell& at(size_t row, size_t col) const = 0;
+	virtual size_t GetRecords() const = 0;
+	virtual size_t GetColumns() const = 0;
+
+	virtual ~BoardAccessor() = default;
+};
+
+class GameBoard : public BoardAccessor {
+private:
+	vector <Cell> Board;
+	size_t columns;
+	size_t records;
+	
+	unique_ptr<Neighborhood> neighborhood;
+	unique_ptr<Boundary> boundary;
+	unique_ptr<LiveRules> rules;
+	
+	
+public:
+	explicit GameBoard(size_t cols, size_t rows, unique_ptr<Neighborhood> init_neighborhood, unique_ptr<Boundary> init_boundary, unique_ptr<LiveRules> init_rules);
+    GameBoard(size_t cols, size_t rows);
+
+
+	Cell& at(size_t r, size_t c);
+
+	const Cell& at(size_t r, size_t c) const override;
+
+	size_t GetColumns() const override;
+
+	size_t GetRecords() const override;
+
+	void ChangeCellStatus(size_t r, size_t c);
+
+	void RewriteFrom(const BufferBoard& copy);
+
+	void checkBoard(BufferBoard& temp);
+
+	void randomGen();
+
+	void reset();
+
+	void setBoundary(unique_ptr<Boundary> newBoundary);
+
+	void setNeighborhood(unique_ptr<Neighborhood> newNeighborhood);
+
+	void setLiveRules(unique_ptr<LiveRules> newRules);
+
+};
+
+
+
+
